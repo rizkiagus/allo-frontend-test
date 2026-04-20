1b6d23ac3c128feaca0c8a13b9345bb5e7ff47c1
Rizki Agus Setiawan <rizki.setiawanagus@gmail.com>
2026-04-21 01:08:30 +0700

    feat: implement error component

---
+++ src/components/Error.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card class="pa-6 text-center">
+    <v-icon size="48" color="error">mdi-alert-circle</v-icon>
+
+    <h3 class="mt-3">Something went wrong</h3>
+    <p>{{ message }}</p>
+
+    <v-btn color="primary" @click="emit('retry')"> Retry </v-btn>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(["retry"]);
+const props = defineProps<{ message: string }>();
+</script>
